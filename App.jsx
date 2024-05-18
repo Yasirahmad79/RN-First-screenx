bbc26af00cc1c69b1ab37172f1a2fbd804586e47
Yasirahmad79 <yasirahmad7939@gmail.com>
2024-05-19 00:56:52 +0500

    add API

--- App.jsx
+++ App.jsx
@@ -1,10 +1,16 @@
+import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import AboutScreen from './src/screens/AboutScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 const App = () => {
+  
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -25,4 +31,17 @@ const App = () => {
   );
 };
 
-export default App;
+const TabNavigator = () => {
+  return (
+    <NavigationContainer>
+    <Tab.Navigator
+      screenOptions={{ tabBarBadge: 3,
+       }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="About" component={AboutScreen} />
+    </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default TabNavigator;
--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,10 +1,36 @@
-import {Text, View} from 'react-native';
+import {Text, View, ActivityIndicator, ScrollView} from 'react-native';
+import {useEffect, useState} from 'react';
 
 const AboutScreen = () => {
+  const [data, setData] = useState([]);
+
+    async function getApiData() {
+    const url = 'https://jsonplaceholder.typicode.com/posts/';
+    let result = await fetch(url);
+    result = await result.json();
+    setData(result);
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 26}}>this is AboutScreen just for checking stack Navigation</Text>
+    <ScrollView>
+    <View style={{flex: 1, alignItems: 'center', flexWrap: 'wrap'}}>
+      <Text style={{fontSize: 26, fontWeight: '900'}}>
+        this is AboutScreen just for checking API calling & stack Navigation!
+      </Text>
+      {data.length
+        ? data.map(item => 
+            <View style={{borderWidth: 2, borderColor: 'purple', margin: 10 , padding: 10}} key={item.id}>
+              <Text style={{fontSize: 15, color: 'red'}}>{item.id}.</Text>
+              <Text style={{fontSize: 25, fontStyle: 'italic',fontWeight: '800'}}>{item.title}.</Text>
+              <Text style={{fontSize: 20}}>{item.body}.</Text>
+            </View>
+          )
+        : <ActivityIndicator size={60} color={'skyblue'} />}
     </View>
+    </ScrollView>
   );
 };
 
